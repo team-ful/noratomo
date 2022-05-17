cf11ce7fa5a10e754ff31d235ee550ebef704672
Yuto Watanabe <yuto.w51942@gmail.com>
2022-05-17 18:56:49 +0900

    dbのテスト削除

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose exec db bash -c "mysql -u docker -pdocker noratomo"
+# docker-compose exec db bash -c "mysql -u docker -pdocker noratomo"
+
+mysql -h 127.0.0.1 -u docker -pdocker noratomo -p
