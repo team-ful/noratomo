98d8b3b599dce8d5c412a6c530269d17b77ce342
Yuto Watanabe <yuto.w51942@gmail.com>
2022-05-24 17:29:45 +0900

    WIP: AuthBase

--- src/base/AuthedBase.ts
+++ src/base/AuthedBase.ts
@@ -1,12 +1,21 @@
+import {Connection, Pool} from 'mysql2/promise';
 import {NextApiRequest, NextApiResponse} from 'next';
+import User from '../models/user';
+import {getUserByUserID} from '../services/user';
 import Base from './base';
 
 /**
  * 認証済みのユーザ用
  */
 class AuthedBase<T> extends Base<T> {
+  private userId: number = NaN;
+
   constructor(req: NextApiRequest, res: NextApiResponse<T>) {
     super(req, res);
+  }
+
+  public async getUser(db: Connection | Pool): Promise<User> {
+    return await getUserByUserID(db, this.userId);
   }
 }
 
--- src/base/base.ts
+++ src/base/base.ts
@@ -1,4 +1,6 @@
+import mysql, {Connection} from 'mysql2/promise';
 import {NextApiRequest, NextApiResponse} from 'next';
+import config from '../../config';
 
 class Base<T> {
   public req: NextApiRequest;
@@ -7,6 +9,18 @@ class Base<T> {
   constructor(req: NextApiRequest, res: NextApiResponse<T>) {
     this.req = req;
     this.res = res;
+  }
+
+  /**
+   * DB Connectionを返す
+   *
+   * @returns {mysql.Connection} DB Connection
+   */
+  public async connectionDB() {
+    const connection = await mysql.createConnection(config.db);
+    await connection.connect();
+
+    return connection;
   }
 }
 
