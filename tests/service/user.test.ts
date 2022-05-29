a9ba71804839c8a9482b5acda845f137a7159532
Yuto Watanabe <yuto.w51942@gmail.com>
2022-05-29 23:47:40 +0900

    WIP: テスト追加

--- tests/service/user.test.ts
+++ tests/service/user.test.ts
@@ -3,28 +3,56 @@ import config from '../../config';
 import {getUserByUserID, createUser} from '../../src/services/user';
 import {createUserModel} from '../../src/tests/user';
 
-describe('ユーザIDから取得', () => {
-  test('connection', async () => {
-    const connection = await mysql.createConnection(config.db);
-    await connection.connect();
+describe('getUser', () => {
+  let connection: mysql.Connection;
 
+  beforeAll(async () => {
+    connection = await mysql.createConnection(config.db);
+    await connection.connect();
+  });
+
+  afterAll(async () => {
+    await connection.end();
+  });
+
+  test('byUserID', async () => {
     const dummy = await createUser(connection, createUserModel());
 
     const user = await getUserByUserID(connection, dummy.id);
 
     expect(user).toEqual(dummy);
-
-    await connection.end();
   }, 10000);
 
-  test('存在しないidはエラーが帰る', async () => {
-    const connection = await mysql.createConnection(config.db);
-    await connection.connect();
-
+  test('byUserIdで存在しないidはエラーが帰る', async () => {
     expect(async () => {
       await getUserByUserID(connection, 123451234512345);
     }).rejects.toThrowError('not found');
+  });
 
+  test('byCateiruSSO', async () => {});
+});
+
+describe('createUser', () => {
+  let connection: mysql.Connection;
+
+  beforeAll(async () => {
+    connection = await mysql.createConnection(config.db);
+    await connection.connect();
+  });
+
+  afterAll(async () => {
     await connection.end();
+  });
+
+  test('作成できる', async () => {
+    const userModel = createUserModel();
+
+    const user = await createUser(connection, userModel);
+
+    expect(user.display_name).toBe(userModel.display_name);
+
+    const dbUser = await getUserByUserID(connection, user.id);
+
+    expect(dbUser).toEqual(user);
   });
 });
