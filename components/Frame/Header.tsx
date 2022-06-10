35c325bbfc7f10eab0d7b882a9fb7fcb30c5797e
Sottiki <81673075+Sottiki@users.noreply.github.com>
2022-06-10 20:16:49 +0900

    ヘッダーにアイコンを追加

--- components/Frame/Header.tsx
+++ components/Frame/Header.tsx
@@ -1,17 +1,33 @@
-import {Box, Flex, Spacer, Heading, Center} from '@chakra-ui/react';
+import {Box, Flex, Spacer, Heading, Center, Avatar} from '@chakra-ui/react';
 import '@fontsource/permanent-marker';
+import {BsBoxSeam} from 'react-icons/bs';
+import {IoMdAddCircleOutline} from 'react-icons/io';
+import {TbHeartHandshake} from 'react-icons/tb';
 
 const Header = () => {
   return (
     <Box width="100%" height="2.5rem" bgColor="blue.100">
       <Flex height="100%">
         <Center height="100%">
-          <Heading fontSize="2rem" fontFamily="Permanent Marker" m=".5rem">
+          <Heading fontSize="2rem" fontFamily="Permanent Marker" ml=".5rem">
             NoraTomo
           </Heading>
         </Center>
         <Spacer />
-        <Box bg="green.400">Box 2</Box>
+        <Flex>
+          <Center>
+            <TbHeartHandshake size="30px" />
+          </Center>
+          <Center>
+            <IoMdAddCircleOutline size="30px" />
+          </Center>
+          <Center>
+            <BsBoxSeam size="30px" />
+          </Center>
+          <Center mr=".5rem">
+            <Avatar size="sm" />
+          </Center>
+        </Flex>
       </Flex>
     </Box>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,18 +25,6 @@ const MyApp = ({Component, pageProps}: AppProps) => {
 
   return (
     <>
-      {/* <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
-          rel="stylesheet"
-        />
-      </Head> */}
       <RecoilRoot>
         <ChakraProvider theme={theme}>
           <Frame>
