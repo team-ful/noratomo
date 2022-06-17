62d49bc1cd4a37d7d2a75c6d6339cb4eaaa2620f
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-17 19:56:26 +0900

    ログインページ作成

---
+++ components/login/Form.tsx
@@ -0,0 +1,132 @@
+import {
+  Box,
+  Center,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Button,
+  Heading,
+  Link,
+  Divider,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {useRouter} from 'next/router';
+import React from 'react';
+import {useForm, SubmitHandler} from 'react-hook-form';
+
+type LoginInputs = {
+  user: string;
+  password: string;
+};
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<LoginInputs>();
+  const router = useRouter();
+  const [load, setLoad] = React.useState(false);
+
+  const onSubmit: SubmitHandler<LoginInputs> = data => {
+    const send = async () => {
+      setLoad(true);
+      const res = await fetch('/api/login/password', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        body: `user=${data.user}&password=${data.password}`,
+      });
+
+      setLoad(false);
+
+      // TODO: ログインできないときになにかしたい
+      if (res.ok) {
+        router.push('/hello');
+      }
+    };
+
+    send();
+  };
+
+  return (
+    <>
+      <Center mt={{base: '2rem', md: '3rem'}}>
+        <Box
+          width={{base: 'auto', md: '400px'}}
+          height="600px"
+          border={{base: 'none', md: '2px solid #CBD5E0'}}
+          borderRadius={{base: 'none', md: '20px'}}
+        >
+          <Center height="100%">
+            <Box width={{base: '95vw', md: '380px'}}>
+              <Heading textAlign="center" mt="2rem">
+                ログイン
+              </Heading>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl isInvalid={Boolean(errors.user)} mt="3rem">
+                  <FormLabel htmlFor="user">
+                    メールアドレスまたはユーザ名
+                  </FormLabel>
+                  <Input
+                    id="user"
+                    type="text"
+                    placeholder="メールアドレスまたはユーザ名"
+                    {...register('user', {
+                      required: 'この項目は必須です',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.user && errors.user.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <FormControl
+                  isInvalid={Boolean(errors.password)}
+                  mt="1rem"
+                  mb=".5rem"
+                >
+                  <FormLabel htmlFor="password">パスワード</FormLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="パスワード"
+                    {...register('password', {
+                      required: 'この項目は必須です',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.password && errors.password.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <NextLink href="/login/forget" passHref>
+                  <Link>パスワードを忘れましたか？</Link>
+                </NextLink>
+                <Button
+                  colorScheme="green"
+                  width="100%"
+                  mt="1.5rem"
+                  type="submit"
+                  isLoading={load}
+                >
+                  ログイン
+                </Button>
+              </form>
+              <Center>
+                <Divider my="1.5rem" width="95%" />
+              </Center>
+              <Center>
+                <NextLink href="/login/forget" passHref>
+                  <Link textAlign="center">アカウントを作成する</Link>
+                </NextLink>
+              </Center>
+            </Box>
+          </Center>
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default LoginForm;
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/login/Form';
+
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
