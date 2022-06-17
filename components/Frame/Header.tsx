1efa6920576d7624829a32c38efde875bd4f4acf
Sottiki <81673075+Sottiki@users.noreply.github.com>
2022-06-17 18:41:23 +0900

    ヘッダーコンポーネントの実装

--- components/Frame/Header.tsx
+++ components/Frame/Header.tsx
@@ -1,12 +1,13 @@
 import {Box, Flex, Spacer, Heading, Center, Avatar} from '@chakra-ui/react';
 import '@fontsource/permanent-marker';
 import {BsBoxSeam} from 'react-icons/bs';
-import {IoMdAddCircleOutline} from 'react-icons/io';
+import {IoChatbubbleOutline} from 'react-icons/io5';
+import {RiAddBoxLine} from 'react-icons/ri';
 import {TbHeartHandshake} from 'react-icons/tb';
 
 const Header = () => {
   return (
-    <Box width="100%" height="2.5rem" bgColor="blue.100">
+    <Box width="100%" height="2.5rem">
       <Flex height="100%">
         <Center height="100%">
           <Heading fontSize="2rem" fontFamily="Permanent Marker" ml=".5rem">
@@ -15,16 +16,19 @@ const Header = () => {
         </Center>
         <Spacer />
         <Flex>
-          <Center>
-            <TbHeartHandshake size="30px" />
+          <Center ml=".5rem">
+            <IoChatbubbleOutline size="26px" />
           </Center>
-          <Center>
-            <IoMdAddCircleOutline size="30px" />
+          <Center ml=".5rem">
+            <TbHeartHandshake size="27px" />
           </Center>
-          <Center>
-            <BsBoxSeam size="30px" />
+          <Center ml=".5rem">
+            <RiAddBoxLine size="27px" />
           </Center>
-          <Center mr=".5rem">
+          <Center ml=".5rem">
+            <BsBoxSeam size="27px" />
+          </Center>
+          <Center mr=".5rem" ml=".5rem">
             <Avatar size="sm" />
           </Center>
         </Flex>
