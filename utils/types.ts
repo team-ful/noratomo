7567bcf3f8421220b7be92ed2936d924902f8f0c
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-21 13:24:05 +0900

    userのrecoil state作成

---
+++ utils/types.ts
@@ -0,0 +1,11 @@
+export interface User {
+  display_name: string;
+  mail: string;
+  profile: string;
+  user_name: string;
+  age: number;
+  gender: number;
+  is_admin: boolean;
+  avatar_url: string;
+  join_date: Date;
+}
