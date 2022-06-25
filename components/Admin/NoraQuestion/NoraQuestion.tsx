3c52ed07a0ee65d34ab7e40a41551b021a6536f2
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-25 19:17:01 +0900

    css修正

--- components/Admin/NoraQuestion/NoraQuestion.tsx
+++ components/Admin/NoraQuestion/NoraQuestion.tsx
@@ -56,100 +56,113 @@ const NoraQuestion = () => {
   return (
     <Box>
       <Heading textAlign="center">野良認証問題</Heading>
-      <Button onClick={createModal.onOpen} colorScheme="blue" mb="1.5rem">
+      <Button
+        onClick={createModal.onOpen}
+        colorScheme="blue"
+        mb="1.5rem"
+        ml="1rem"
+      >
         新規作成
       </Button>
-      <Skeleton isLoaded={!getLoad}>
-        <TableContainer>
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th>ID</Th>
-                <Th>タイトル</Th>
-                <Th>スコア</Th>
-                <AnswerScoreTh />
-              </Tr>
-            </Thead>
-            <Tbody>
-              {questions.map((v, i) => {
-                return (
-                  <Tr key={`tr_key_${i}`}>
-                    <Td>
-                      <Link
-                        as="p"
-                        onClick={() => {
-                          setSelected(v);
-                          detailsModal.onOpen();
-                        }}
-                      >
-                        {v.id}
-                      </Link>
-                    </Td>
-                    <Td>{v.question_title}</Td>
-                    <Td>{v.score}</Td>
-                    {v.answers.map((a, i) => {
-                      return i === v.current_answer_index ? (
-                        <Td key={`td_key_${i}`} bgColor="red.300">
-                          {a.answerText}
-                        </Td>
-                      ) : (
-                        <Td key={`td_key_${i}`}>{a.answerText}</Td>
-                      );
-                    })}
-                  </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Skeleton>
-      <UpdateNoraQuestion
-        onClose={detailsModal.onClose}
-        isOpen={detailsModal.isOpen}
-        onCloseModalOpen={deleteModal.onOpen}
-        defaultQuestion={selected}
-        update={updateQuestion}
-      />
-      {/* 野良認証問題を削除するモーダル */}
-      <Modal
-        isOpen={deleteModal.isOpen}
-        onClose={deleteModal.onClose}
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>ID: {selected?.id} を削除しますか？</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>削除するともとに戻すことはできません。</ModalBody>
+      <Box overflow="scroll">
+        <Box width="100vw">
+          <Skeleton isLoaded={!getLoad} overflowX="scroll">
+            <Table variant="simple" minWidth="1000px">
+              <Thead>
+                <Tr>
+                  <Th>ID</Th>
+                  <Th>タイトル</Th>
+                  <Th>スコア</Th>
+                  <AnswerScoreTh />
+                </Tr>
+              </Thead>
+              <Tbody>
+                {questions.map((v, i) => {
+                  return (
+                    <Tr key={`tr_key_${i}`}>
+                      <Td>
+                        <Link
+                          as="p"
+                          onClick={() => {
+                            setSelected(v);
+                            detailsModal.onOpen();
+                          }}
+                        >
+                          {v.id}
+                        </Link>
+                      </Td>
+                      <Td minW="100px">{v.question_title}</Td>
+                      <Td>{v.score}</Td>
+                      {v.answers.map((a, i) => {
+                        return i === v.current_answer_index ? (
+                          <Td
+                            key={`td_key_${i}`}
+                            bgColor="red.300"
+                            minW="100px"
+                          >
+                            {a.answerText}
+                          </Td>
+                        ) : (
+                          <Td key={`td_key_${i}`} minW="100px">
+                            {a.answerText}
+                          </Td>
+                        );
+                      })}
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </Skeleton>
+        </Box>
+        <UpdateNoraQuestion
+          onClose={detailsModal.onClose}
+          isOpen={detailsModal.isOpen}
+          onCloseModalOpen={deleteModal.onOpen}
+          defaultQuestion={selected}
+          update={updateQuestion}
+        />
+        {/* 野良認証問題を削除するモーダル */}
+        <Modal
+          isOpen={deleteModal.isOpen}
+          onClose={deleteModal.onClose}
+          isCentered
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>ID: {selected?.id} を削除しますか？</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>削除するともとに戻すことはできません。</ModalBody>
 
-          <ModalFooter>
-            <Button
-              colorScheme="red"
-              mr=".5rem"
-              onClick={() => {
-                if (selected?.id) {
-                  const f = async () => {
-                    await removeQuestion(selected.id);
-                    deleteModal.onClose();
-                  };
+            <ModalFooter>
+              <Button
+                colorScheme="red"
+                mr=".5rem"
+                onClick={() => {
+                  if (selected?.id) {
+                    const f = async () => {
+                      await removeQuestion(selected.id);
+                      deleteModal.onClose();
+                    };
 
-                  f();
-                }
-              }}
-            >
-              削除
-            </Button>
-            <Button mr={3} onClick={deleteModal.onClose}>
-              閉じる
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      <CreateNewNoraQuestion
-        isOpen={createModal.isOpen}
-        onClose={createModal.onClose}
-        create={newQuestion}
-      />
+                    f();
+                  }
+                }}
+              >
+                削除
+              </Button>
+              <Button mr={3} onClick={deleteModal.onClose}>
+                閉じる
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        <CreateNewNoraQuestion
+          isOpen={createModal.isOpen}
+          onClose={createModal.onClose}
+          create={newQuestion}
+        />
+      </Box>
     </Box>
   );
 };
