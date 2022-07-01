5f3ab0d37e631aadecaee9d04daed8d00914adc3
deleter <18nc060@ms.dendai.ac.jp>
2022-07-01 19:14:46 +0900

    ボーダーをデリートした、ログインボタンを蒼にした

--- components/login/Form.tsx
+++ components/login/Form.tsx
@@ -52,69 +52,67 @@ const LoginForm = () => {
   };
 
   return (
-          <Center>
-            <Box mt="2rem" w={{base: '96%', md: '500px'}}>
-              <Heading textAlign="center" mb="1rem">
-                ログイン
-              </Heading>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl isInvalid={Boolean(errors.user)} mt="3rem">
-                  <FormLabel htmlFor="user">
-                    メールアドレスまたはユーザ名
-                  </FormLabel>
-                  <Input
-                    id="user"
-                    type="text"
-                    placeholder="メールアドレスまたはユーザ名"
-                    {...register('user', {
-                      required: 'この項目は必須です',
-                    })}
-                  />
-                  <FormErrorMessage>
-                    {errors.user && errors.user.message}
-                  </FormErrorMessage>
-                </FormControl>
-                <FormControl
-                  isInvalid={Boolean(errors.password)}
-                  mt="1rem"
-                  mb=".5rem"
-                >
-                  <FormLabel htmlFor="password">パスワード</FormLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    placeholder="パスワード"
-                    {...register('password', {
-                      required: 'この項目は必須です',
-                    })}
-                  />
-                  <FormErrorMessage>
-                    {errors.password && errors.password.message}
-                  </FormErrorMessage>
-                </FormControl>
-                <NextLink href="/login/forget" passHref>
-                  <Link>パスワードを忘れましたか？</Link>
-                </NextLink>
-                <Button
-                  colorScheme="blue"
-                  width="100%"
-                  mt="1.5rem"
-                  type="submit"
-                  isLoading={load}
-                >
-                  ログイン
-                </Button>
-              </form>
-              <Center>
-                <Divider my="1.5rem" width="95%" />
-              </Center>
-              <Center>
-                <NextLink href="/login/forget" passHref>
-                  <Link textAlign="center">アカウントを作成する</Link>
-                </NextLink>
-              </Center>
-            </Box>
-          </Center>
+    <Center>
+      <Box mt="2rem" w={{base: '96%', md: '500px'}}>
+        <Heading textAlign="center" mb="1rem">
+          ログイン
+        </Heading>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={Boolean(errors.user)} mt="3rem">
+            <FormLabel htmlFor="user">メールアドレスまたはユーザ名</FormLabel>
+            <Input
+              id="user"
+              type="text"
+              placeholder="メールアドレスまたはユーザ名"
+              {...register('user', {
+                required: 'この項目は必須です',
+              })}
+            />
+            <FormErrorMessage>
+              {errors.user && errors.user.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl
+            isInvalid={Boolean(errors.password)}
+            mt="1rem"
+            mb=".5rem"
+          >
+            <FormLabel htmlFor="password">パスワード</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="パスワード"
+              {...register('password', {
+                required: 'この項目は必須です',
+              })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+          <NextLink href="/login/forget" passHref>
+            <Link>パスワードを忘れましたか？</Link>
+          </NextLink>
+          <Button
+            colorScheme="blue"
+            width="100%"
+            mt="1.5rem"
+            type="submit"
+            isLoading={load}
+          >
+            ログイン
+          </Button>
+        </form>
+        <Center>
+          <Divider my="1.5rem" width="95%" />
+        </Center>
+        <Center>
+          <NextLink href="/login/forget" passHref>
+            <Link textAlign="center">アカウントを作成する</Link>
+          </NextLink>
+        </Center>
+      </Box>
+    </Center>
   );
 };
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,9 @@
+import Require from '../components/Session/Require';
 import LoginForm from '../components/login/Form';
-import Require from '../components/Session/Require'
 
 const Login = () => {
   return (
-      <Require loginRequire={false} path="/">
+    <Require loginRequire={false} path="/">
       <LoginForm />
     </Require>
   );
