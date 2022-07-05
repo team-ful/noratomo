f27226e8f45033cd61b9abc8a825cff1ceab5c43
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-05 17:52:47 +0900

    dbをまとめるクラス作成

---
+++ src/db/operator.ts
@@ -0,0 +1,105 @@
+import sql from 'mysql-bricks';
+import {
+  Connection,
+  RowDataPacket,
+  OkPacket,
+  ResultSetHeader,
+} from 'mysql2/promise';
+
+export type QueryPacket =
+  | RowDataPacket[]
+  | RowDataPacket[][]
+  | OkPacket
+  | OkPacket[]
+  | ResultSetHeader;
+
+export interface DefaultObject {
+  [key: string]: string | Date | number | boolean | null;
+}
+
+export default class DBOperator {
+  private db: Connection;
+
+  constructor(db: Connection) {
+    this.db = db;
+  }
+
+  /**
+   * DBクエリを実行する
+   *
+   * @param {sql.SqlBricksParam} query - クエリ
+   * @returns {QueryPacket} - クエリ
+   */
+  public async runQuery<T extends QueryPacket>(
+    query: sql.SqlBricksParam
+  ): Promise<T> {
+    const [rows] = await this.db.query<T>(query.text, query.values);
+
+    return rows;
+  }
+
+  /**
+   * DBから要素を1つ取得する
+   *
+   * @param {sql.SqlBricksParam} query - クエリ
+   * @returns {null | object}  要素が無い場合はnull
+   */
+  public async one<T extends Object = DefaultObject>(
+    query: sql.SqlBricksParam
+  ): Promise<null | T> {
+    const row = await this.runQuery<RowDataPacket[]>(query);
+
+    if (row.length === 0) {
+      return null;
+    }
+
+    return row[0] as T;
+  }
+
+  /**
+   * DBから複数の要素を取得する
+   *
+   * @param {sql.SqlBricksParam} query - クエリ
+   * @returns {null | object}  要素が無い場合はnull
+   */
+  public async multi<T extends Object = DefaultObject>(
+    query: sql.SqlBricksParam
+  ): Promise<null | T[]> {
+    const row = await this.runQuery<RowDataPacket[]>(query);
+
+    if (row.length === 0) {
+      return null;
+    }
+
+    return row as T[];
+  }
+
+  /**
+   * insertをして、insertIdを返します
+   *
+   * @param {sql.SqlBricksParam} query - クエリ
+   * @returns {number}  insert id
+   */
+  public async insert(query: sql.SqlBricksParam): Promise<number> {
+    const row = await this.runQuery<ResultSetHeader>(query);
+
+    return row.insertId;
+  }
+
+  /**
+   * ただ単に実行する
+   *
+   * @param {sql.SqlBricksParam} query - クエリ
+   */
+  public async execute(query: sql.SqlBricksParam): Promise<void> {
+    await this.runQuery(query);
+  }
+
+  /**
+   * DBを終了する
+   *
+   */
+  public async close() {
+    await this.db.end();
+  }
+}
