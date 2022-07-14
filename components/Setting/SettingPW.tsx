b10780ac24b583f8ce17b0c0b3333e354b68723c
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-14 21:57:49 +0900

    パスワード更新ページ修正

--- components/Setting/SettingPW.tsx
+++ components/Setting/SettingPW.tsx
@@ -1,55 +1,102 @@
 import {
-  Button,
   FormControl,
   FormLabel,
-  Heading,
   Input,
   Center,
   Box,
+  Button,
+  useToast,
+  FormErrorMessage,
+  InputGroup,
+  IconButton,
+  InputRightElement,
 } from '@chakra-ui/react';
-import {useForm} from 'react-hook-form';
+import React from 'react';
+import {SubmitHandler, useForm, FormProvider} from 'react-hook-form';
+import {TbEye, TbEyeOff} from 'react-icons/tb';
+import Password, {PasswordForm} from '../Common/Form/Password';
 
-type SettingInputs = {
+interface PasswordChangeForm extends PasswordForm {
   now_password: string;
-  new_password: string;
-};
+}
 
 const SettingPW = () => {
+  const methods = useForm<PasswordChangeForm>();
   const {
-    // register,
-    // handleSubmit,
-    // setValue,
-    formState: {isSubmitting, isValid, isDirty},
-  } = useForm<SettingInputs>();
-  // const [user, setUser] = useRecoilState(UserState);
-  // const toast = useToast();
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: {errors, isSubmitting},
+  } = methods;
+
+  const toast = useToast();
+
+  const [pwOk, setPWOk] = React.useState(false);
+  const [show, setShow] = React.useState(false);
+
+  const onSubmit: SubmitHandler<PasswordChangeForm> = async data => {
+    if (!pwOk) {
+      setError('password', {
+        type: 'custom',
+        message: 'custom message',
+      });
+      return;
+    } else {
+      clearErrors('password');
+    }
+
+    toast({
+      title: 'TODO: パスワード更新できるようにする',
+      description: data.password,
+      status: 'info',
+    });
+  };
 
   return (
     <Center>
       <Box mt="2rem" w={{base: '97%', sm: '400px', md: '500px'}}>
-        <Heading textAlign="center" mb="1rem" size="lg">
-          パスワードパスワード変更
-        </Heading>
-        <form>
-          <FormControl mb="1rem" isDisabled>
-            <FormLabel>認証のため現在のパスワードを入力して下さい</FormLabel>
-            <Input id="" type="" placeholder="現在のパスワードを入力" />
-          </FormControl>
-
-          <FormControl mb="1rem" isDisabled>
-            <FormLabel>新しいパスワードを設定</FormLabel>
-            <Input id="" type="" placeholder="新しいパスワードを入力" />
-          </FormControl>
-          <Button
-            disabled={true}
-            isLoading={isSubmitting}
-            type="submit"
-            size="lg"
-            marginTop="2rem"
-          >
-            保存
-          </Button>
-        </form>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={Boolean(errors.now_password)}>
+              <FormLabel>現在のパスワード</FormLabel>
+              <InputGroup>
+                <Input
+                  id="now_password"
+                  type={show ? 'text' : 'password'}
+                  placeholder="パスワード"
+                  {...register('now_password', {
+                    required: 'パスワードは必須です',
+                  })}
+                />
+                <InputRightElement>
+                  <IconButton
+                    variant="ghost"
+                    aria-label="show password"
+                    icon={
+                      show ? <TbEye size="25px" /> : <TbEyeOff size="25px" />
+                    }
+                    size="sm"
+                    onClick={() => setShow(!show)}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>
+                {errors.now_password && errors.now_password.message}
+              </FormErrorMessage>
+            </FormControl>
+            <Password setOk={setPWOk}>新しいパスワード</Password>
+            <Button
+              isLoading={isSubmitting}
+              type="submit"
+              marginTop="2rem"
+              colorScheme="orange"
+              width="100%"
+            >
+              更新
+            </Button>
+          </form>
+        </FormProvider>
       </Box>
     </Center>
   );
--- pages/setting/password.tsx
+++ pages/setting/password.tsx
@@ -1,10 +1,11 @@
 import type {NextPage} from 'next';
 import Require from '../../components/Session/Require';
+import SettingPW from '../../components/Setting/SettingPW';
 
 const Password: NextPage = () => {
   return (
     <Require loginRequire={true} path="/">
-      password
+      <SettingPW />
     </Require>
   );
 };
