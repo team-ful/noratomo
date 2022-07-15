ecbc5a294291a2a24ecbfcc88fa93d5a7829e785
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-15 23:55:02 +0900

    section2作成

--- components/Top/Top.tsx
+++ components/Top/Top.tsx
@@ -1,9 +1,11 @@
 import Section1 from './Section1';
+import Section2 from './Section2';
 
 const Top = () => {
   return (
     <>
       <Section1 />
+      <Section2 />
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type {NextPage} from 'next';
-import Require from '../components/Session/Require';
 import Top from '../components/Top/Top';
 
 const Home: NextPage = () => {
-  return (
-    <Require loginRequire={false} path="/">
-      <Top />
-    </Require>
-  );
+  return <Top />;
 };
 
 export default Home;
