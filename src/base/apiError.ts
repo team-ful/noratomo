2e2e6a3fcbec7bb5862aae511b18540773de4f64
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-31 00:08:19 +0900

    修正

--- src/base/apiError.ts
+++ src/base/apiError.ts
@@ -16,8 +16,10 @@ export class ApiError extends Error {
   }
 
   public sendRes<T>(res: NextApiResponse<T>) {
-    res.status(this.code).send(this.message as any);
-    res.end();
+    // ref. https://github.com/vercel/next.js/blob/881c313b39dc2b975d6c5da89f223a6b2ca6c541/packages/next/server/api-utils/index.ts#L163-L171
+    res.statusCode = this.code;
+    res.statusMessage = this.message;
+    res.end(this.message);
 
     if (process.env.NODE_ENV !== 'production') {
       console.error(this.stack);
