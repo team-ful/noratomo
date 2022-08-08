6f1b90a5bffb34033abc062dba143a11b15e74cb
Yuto Watanabe <yuto.w51942@gmail.com>
2022-08-09 02:18:53 +0900

    店の型定義

--- utils/types.ts
+++ utils/types.ts
@@ -23,3 +23,62 @@ export interface AdminUser extends User {
   is_ban: boolean;
   is_penalty: boolean;
 }
+
+export interface Shop {
+  // お店ID
+  id: string;
+
+  // 掲載店名
+  name: string;
+  address: string;
+
+  // 位置
+  lat: number;
+  lng: number;
+
+  // 料金備考
+  budget_memo: string;
+  // その他設備
+  other_memo: string;
+  // 備考
+  shop_detail_memo: string;
+  photo: {
+    pc: {
+      l: string;
+      m: string;
+      s: string;
+    };
+  };
+
+  // ジャンル
+  genre: {
+    // ジャンル名
+    name: string;
+    // ジャンルキャッチ
+    catch: string;
+  };
+  // キャッチ
+  catch: string;
+  // 交通アクセス
+  access: string;
+  // 店舗URL
+  urls: {
+    // PC向け
+    pc: string;
+  };
+}
+
+export interface Shops {
+  results: {
+    api_version: string;
+
+    // 検索結果の全件数
+    results_available: number;
+    // 返される結果の件数
+    results_returned: string;
+    // 検索結果の開始位置
+    results_start: number;
+
+    shop: Shop[];
+  };
+}
