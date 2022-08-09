dfaa276f169b7ae3a233908db9cd132bf3f0efbe
Yuto Watanabe <yuto.w51942@gmail.com>
2022-08-09 19:14:46 +0900

    create

--- components/Entry/Search.tsx
+++ components/Entry/Search.tsx
@@ -2,6 +2,7 @@ import {Box, Center, VStack, Text} from '@chakra-ui/react';
 import {useRouter} from 'next/router';
 import React from 'react';
 import Pagination from './Pagination';
+import SearchInit from './SearchInit';
 import SearchKeywordForm from './SearchKeyword';
 import ShopContent from './ShopContent';
 import useShopSearch from './useShopSearch';
@@ -18,6 +19,7 @@ const SearchShop = () => {
   } = useShopSearch();
 
   const [page, setPage] = React.useState(1);
+  const [init, setInit] = React.useState(true);
 
   React.useEffect(() => {
     if (!router.isReady) return;
@@ -28,12 +30,14 @@ const SearchShop = () => {
       setPage(_page);
     }
 
-    // keyword検索
-    if (typeof router.query['keyword'] === 'string') {
-      searchQuery(router.query['keyword'], _page);
-    }
-
+    // 検索
     if (
+      typeof router.query['keyword'] === 'string' &&
+      router.query['keyword'] !== ''
+    ) {
+      searchQuery(router.query['keyword'], _page);
+      setInit(true);
+    } else if (
       typeof router.query['lat'] === 'string' &&
       typeof router.query['lon'] === 'string' &&
       typeof router.query['range'] === 'string'
@@ -44,26 +48,35 @@ const SearchShop = () => {
         parseInt(router.query['range']),
         _page
       );
+      setInit(true);
+    } else {
+      setInit(false);
     }
   }, [router.isReady, router.query]);
 
   return (
-    <Center w="100%">
-      <VStack w="100%">
-        <SearchKeywordForm />
-        <Box w="100%">
-          <Text textAlign="right">
-            検索結果: {20 * page + resultsReturned}/{resultsAvailable}件
-          </Text>
-        </Box>
-        <Box w="100%">
-          {shops.map(v => {
-            return <ShopContent key={v.id} shop={v} />;
-          })}
-        </Box>
-        <Pagination all={resultsAvailable} current={page} />
-      </VStack>
-    </Center>
+    <>
+      {init ? (
+        <Center w="100%">
+          <VStack w="100%" mx=".5rem">
+            <SearchKeywordForm />
+            <Box w="100%">
+              <Text textAlign="right">
+                検索結果: {20 * page + resultsReturned}/{resultsAvailable}件
+              </Text>
+            </Box>
+            <Box w="100%">
+              {shops.map(v => {
+                return <ShopContent key={v.id} shop={v} />;
+              })}
+            </Box>
+            <Pagination all={resultsAvailable} current={page} />
+          </VStack>
+        </Center>
+      ) : (
+        <SearchInit />
+      )}
+    </>
   );
 };
 
