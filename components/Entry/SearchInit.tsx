c1327cff3fcd988ee6ea66da104f2417ab756943
Yuto Watanabe <yuto.w51942@gmail.com>
2022-08-10 23:25:11 +0900

    幅修正

--- components/Entry/SearchInit.tsx
+++ components/Entry/SearchInit.tsx
@@ -5,12 +5,15 @@ import SearchLatLon from './SearchLatLon';
 const SearchInit = () => {
   return (
     <Center h="80vh" w="100%">
-      <Box>
-        <Heading mb="1rem">行きたいお店を検索しよう</Heading>
+      <Box w="100%" mt="3rem">
+        <Heading mb="1rem" textAlign="center">
+          行きたいお店を検索しよう
+        </Heading>
         <SearchKeywordForm />
         <Text my="1.5rem" textAlign="center">
           もしくは、
         </Text>
+
         <SearchLatLon
           style={{width: '100%', height: '400px'}}
           center={{
