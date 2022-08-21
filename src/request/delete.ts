3514a0b1f9aeea97988e28cba7863235bff50349
Yuto Watanabe <yuto.w51942@gmail.com>
2022-08-21 17:13:56 +0900

    修正

--- src/request/delete.ts
+++ src/request/delete.ts
@@ -2,6 +2,7 @@ import {ApiError} from '../base/apiError';
 import AuthedBase from '../base/authedBase';
 import {authHandlerWrapper} from '../base/handlerWrapper';
 import {deleteApplicationByUserIdAndEntryId} from '../services/application';
+import {findEntryById} from '../services/entry';
 import {updateNumberOf} from '../services/numberOf';
 
 /**
@@ -14,6 +15,11 @@ async function deleteRequestHandler(base: AuthedBase<void>) {
   const numberId = parseInt(id);
   if (isNaN(numberId)) {
     throw new ApiError(400, 'id is failed');
+  }
+
+  const entry = await findEntryById(await base.db(), numberId);
+  if (entry?.is_closed) {
+    throw new ApiError(400, 'entry is already closed');
   }
 
   await deleteApplicationByUserIdAndEntryId(
