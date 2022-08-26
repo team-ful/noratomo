7a0b2842169a2cfed44f4803a079f8933533a8e6
Sottiki <81673075+Sottiki@users.noreply.github.com>
2022-08-26 18:31:03 +0900

    ハンドラー login_history.test.ts 実装途中

--- tests/pages/api/user/login_history.test.ts
+++ tests/pages/api/user/login_history.test.ts
@@ -1,5 +1,54 @@
+import {send} from 'process';
 import {testApiHandler} from 'next-test-api-route-handler';
+import {BaseNextRequest} from 'next/dist/server/base-http';
 import login_historyHandler from '../../../../pages/api/user/login_history';
+import AuthedBase from '../../../../src/base/base';
+import {findLoginHistoriesByUserID} from '../../../../src/services/loginHistory';
 import TestBase from '../../../../src/tests/base';
 
-describe('login_history', () => {});
+/**
+ *@param {AuthedBase<void>} bbase -bbase
+ */
+
+describe('login_history', () => {
+  const base = new TestBase();
+
+  beforeAll(async () => {
+    await base.connection();
+
+    const u = await base.newUser();
+    await u.loginFromPassword(base.db);
+    await u.addSession(base.db);
+  });
+
+  afterAll(async () => {
+    await base.end();
+  });
+
+  test('login_history', async () => {
+    expect.hasAssertions();
+
+    await testApiHandler({
+      handler: login_historyHandler,
+      requestPatcher: async req => {
+        req.headers = {
+          cookie: base.users[0].sessionCookie,
+        };
+      },
+      test: async ({fetch}) => {
+        const res = await fetch();
+
+        expect(res.status).toBe(200);
+        if (res.status && base.users[0].user?.id) {
+          const findedLoginHistory = await findLoginHistoriesByUserID(
+            base.db,
+            base.users[0].user?.id
+          );
+          const d = findedLoginHistory.map(x => x.json());
+
+          expect(await res.json()).toEqual(d);
+        }
+      },
+    });
+  });
+});
