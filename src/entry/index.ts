8195b9ccf82f7a2f14ef9739ea2a6418b78c0ea0
Yuto Watanabe <yuto.w51942@gmail.com>
2022-09-02 13:06:06 +0900

    コメント追加

--- src/entry/index.ts
+++ src/entry/index.ts
@@ -1,3 +1,4 @@
+// 募集を作成したり作成した募集を返したりする
 import get from './get';
 import getAll from './getAll';
 import post from './post';
--- src/request/index.ts
+++ src/request/index.ts
@@ -1,3 +1,10 @@
+// 募集に対して「いいね」をしたり削除したりする
+// 「いいね」の呼び方は定まっていないため複数存在します
+// - いいね
+// - request
+// - application
+// TODO: 直したい
+
 import _delete from './delete';
 import get from './get';
 import post from './post';
